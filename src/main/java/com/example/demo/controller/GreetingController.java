6c071fd1b075941caee1474d06237f1089f0cd95
herambha kumar <herambhakumar@gmail.com>
2023-03-31 11:19:44 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	@GetMapping("sayHello")
+	public String sayHello() {
+		return  "It is wowrking fine !";
+	}
+
+	@Override
+	public String toString() {
+		return "GreetingController []";
+	}
+
+}
